818072674156e2bd138a92a86cb6ef55f6506322
francis-chris5 <50467171+francis-chris5@users.noreply.github.com>
2022-08-30 20:00:00 -0400

    Add files via upload

---
+++ newFolder/Primitives.java
@@ -0,0 +1,31 @@
+
+package primitives;
+
+
+public class Primitives {
+
+    public static void main(String[] args) {
+        byte b1 = 123;
+        short s1 = 14567;
+        int i = 145678;
+        long big = 1234567898765L;
+        
+        double dub = 1.07;
+        float flt = 1.07f;
+        
+        boolean flag = true;
+        boolean otherFlag = false;
+        
+        char startOfAlphebet = 'A';
+        String someWords = "Hello World";
+        
+        System.out.println("8 bits of data to store " + b1 + ".");
+        System.out.println("16 bits of data to store " + s1);
+        System.out.println("32 bits of data to store " + i);
+        System.out.println("64 bits of data to store " + big);
+        
+        System.out.println("a 64 bit real number " + dub);
+        
+    }
+    
+}
